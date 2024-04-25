ea2331c0d6426d0c9f4bba0de6152a96c20fb129
endoeduardo <eduardo.t.endo@gmail.com>
2024-04-24 21:09:58 -0300

    [ADD]: Adição da pasta de queries construídas ao longo do curso

---
+++ SQL/aula_1.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS aluno (
+	id SERIAL PRIMARY KEY,
+	primeiro_nome VARCHAR(255) NOT NULL,
+	ultimo_nome VARCHAR(255) NOT NULL,
+	data_nascimento DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS categoria (
+	id 	SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS curso (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL UNIQUE,
+	catetgoria_id INTEGER NOT NULL REFERENCES categoria(id)
+);
+
+CREATE TABLE IF NOT EXISTS aluno_curso (
+	aluno_id INTEGER NOT NULL REFERENCES aluno(id),
+	curso_id INTEGER NOT NULL REFERENCES curso(id),
+	PRIMARY KEY (aluno_id, curso_id)
+);
